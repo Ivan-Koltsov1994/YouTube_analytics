d72802a940af323a9d9911dfe7e2b197302c4aa3
Ivan <ivan.koltsov.1994@mail.com>
2023-03-03 23:45:01 +0500

    Модифицирован метод __init__, id канала нельзя было его изменять/удалять, реализован метод классаget_service, который возвращает объект для работы с API ютуб, реализован методrecord_to_json, который сохраняет информацию по каналу, хранящуюся в атрибутах экземпляра класса Channel, в json-файл

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from src.Dataclass import Channel
 
 channel_vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')  # id канала Вдудь
-channel_news = Channel('UC1eFXmJNkjITxPFWTy6RsWg')  # id редакции
 
-channel_vdud .print_info()
-channel_news.print_info()
+channel_vdud .print_info() # информация о канале
+
+print(channel_vdud.title)
+print(channel_vdud.video_count)
+print(channel_vdud.url)
+channel_vdud.channel_id = 'new_id'
+print(Channel.get_service())
+channel_vdud.record_to_json('vdud.json')
