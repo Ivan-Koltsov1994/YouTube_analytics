b3d9fdc4ff77d94e68af20cda7580ad2f9a116fa
Ivan <ivan.koltsov.1994@mail.com>
2023-03-05 18:49:10 +0500

    Окончательный вариант Задания 3

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from src.Dataclass import Channel
 
 channel_vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')  # id канала Вдудь
 channel_news = Channel('UC1eFXmJNkjITxPFWTy6RsWg')  # id канала Редакции
-#channel_vdud .print_info() # информация о канале
+# channel_vdud .print_info() # информация о канале
 
-#print(channel_vdud.title)
-#print(channel_vdud.video_count)
-#print(channel_vdud.url)
-#channel_vdud.channel_id = 'new_id'
-#print(Channel.get_service())
-#channel_vdud.record_to_json('vdud.json')
-#print(channel_vdud.print_info())
+# print(channel_vdud.title)
+# print(channel_vdud.video_count)
+# print(channel_vdud.url)
+# channel_vdud.channel_id = 'new_id'
+# print(Channel.get_service())
+# channel_vdud.record_to_json('vdud.json')
+channel_vdud.print_info()
 
 print(channel_vdud)
 print(channel_news)
